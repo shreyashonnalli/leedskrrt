7ce9e906a0ae3687eeea5f89a5374f21f5480b56
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2022-03-11 03:24:38 +0000

    Manually merging Sultans code due to git issues. Code checked line by line as with Lucas

--- app/models.py
+++ app/models.py
@@ -2,26 +2,25 @@ from app import db
 from flask_login import UserMixin
 
 
-# Customer account model
-class Customer(db.Model, UserMixin):
+# Account model which stores both the customer and manager accounts
+class Account(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
+    role = db.Column(db.Integer, primary_key=False)
 
-class Manager(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
 
 class Scooter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String)
     availability = db.Column(db.Boolean, default = True)
 
+
 class Options(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hours = db.Column(db.Integer)
     price = db.Column(db.Integer)
+
 
 class Booking(db.Model):
     bookingId = db.Column(db.Integer, primary_key=True)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, request, redirect, url_for, session
 from app import app, db, bcrypt
 from .forms import RegisterForm, LoginForm, ScooterForm, OptionsForm, PaymentForm, RegisterManagerForm, AddPaymentMethodForm, FeedbackForm
-from .models import Customer, Scooter, Options, Booking, Manager, PaymentCard, FeedbackCard
+from .models import Account, Scooter, Options, Booking, PaymentCard, FeedbackCard
 from flask_login import login_user, login_required, logout_user, current_user, LoginManager
 from datetime import timedelta, datetime
 import datetime as dt
@@ -31,9 +31,11 @@ def before_request():
     session.permanent = True
     app.permanent_session_lifetime = timedelta(minutes=60 * 2)
 
+
+
 @login_manager.user_loader
 def load_user(id):
-    return Customer.query.get(int(id))
+    return Account.query.get(int(id))
 
 
 # Register view
@@ -43,15 +45,18 @@ def register():
 
     if form.validate_on_submit():
         email = form.email.data
-        user_exists = Customer.query.filter_by(email=email).first()
+        user_exists = Account.query.filter_by(email=email).first()
         if user_exists:
             flash('This email has already been used', category='error')
         else:
+            # A customer account is indicated by its role number which is 1.
+            # This will be used to restrict customer from accessing manager views
+            role = 1
             password = form.password.data
 
             # Encrypts password using bcrypt, this is so the password is not shown as plain-text in the database.
             hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-            new_customer = Customer(email=email, password=hashed_password)
+            new_customer = Account(email=email, password=hashed_password, role=role)
             # Adds the customer account to the database
             db.session.add(new_customer)
             db.session.commit()
@@ -59,29 +64,34 @@ def register():
             return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/registermanager', methods=['GET', 'POST'])
 def registermanager():
     form = RegisterManagerForm()
 
     if form.validate_on_submit():
         email = form.email.data
-        user_exists = Manager.query.filter_by(email=email).first()
+        user_exists = Account.query.filter_by(email=email).first()
         if user_exists:
             flash('This email has already been used', category='error')
         elif form.managerPassword.data != "asdfghjk":
             flash('Manager password incorrect', category='error')
         else:
+            # A manager account is indicated by its role number which is 2.
+            # This will be used to restrict manager from accessing customer views
+            role = 2
             password = form.password.data
 
             # Encrypts password using bcrypt, this is so the password is not shown as plain-text in the database.
             hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-            new_manager = Manager(email=email, password=hashed_password)
+            new_manager = Account(email=email, password=hashed_password, role=role)
             # Adds the customer account to the database
             db.session.add(new_manager)
             db.session.commit()
             flash('Successfully created account!', category='success')
             return redirect(url_for('login'))
     return render_template('ManagerRegister.html', title='Register', form=form)
+
 
 # Login view
 @app.route('/login', methods=['GET', 'POST'])
@@ -90,39 +100,38 @@ def login():
 
     if form.validate_on_submit():
         # Searches database for the inputted email name
-        customerAccount = Customer.query.filter_by(email=form.email.data).first()
-        managerAccount = Manager.query.filter_by(email=form.email.data).first()
+        account = Account.query.filter_by(email=form.email.data).first()
         # If the user has been found, then the following code is executed
-        if customerAccount:
+        if account and account.role == 1:
             # Checks to see if password from database matches the inputted password
-            if bcrypt.check_password_hash(customerAccount.password, form.password.data):
+            if bcrypt.check_password_hash(account.password, form.password.data):
                 flash("Successfully logged in!", category='success')
                 # If user has ticked remember me checkbox, then a remember me cookie is created locally
                 if form.remember.data:
                     # Cookie will stay alive for 14 days, once expired the user will need to sign in again
                     # If user signs out manually or clears cookies from browser then the user will need to sign in again
-                    login_user(customerAccount, remember=True, duration=timedelta(days=14))
+                    login_user(account, remember=True, duration=timedelta(days=14))
                     return redirect(url_for('index'))
                 else:
                     # No cookies used, instead a session with a set duration is used
-                    login_user(customerAccount, remember=False)
+                    login_user(account, remember=False)
                     return redirect(url_for('index'))
             else:
                 # If the password is incorrect, error message is displayed
                 flash('Email or password is incorrect', category='error')
-        elif managerAccount:
+        elif account and account.role == 2:
             # Checks to see if password from database matches the inputted password
-            if bcrypt.check_password_hash(managerAccount.password, form.password.data):
+            if bcrypt.check_password_hash(account.password, form.password.data):
                 flash("Successfully logged in!", category='success')
                 # If user has ticked remember me checkbox, then a remember me cookie is created locally
                 if form.remember.data:
                     # Cookie will stay alive for 14 days, once expired the user will need to sign in again
                     # If user signs out manually or clears cookies from browser then the user will need to sign in again
-                    login_user(managerAccount, remember=True, duration=timedelta(days=14))
+                    login_user(account, remember=True, duration=timedelta(days=14))
                     return redirect(url_for('managerindex'))
                 else:
                     # No cookies used, instead a session with a set duration is used
-                    login_user(managerAccount, remember=False)
+                    login_user(account, remember=False)
                     return redirect(url_for('managerindex'))
             else:
                 # If the password is incorrect, error message is displayed
@@ -152,106 +161,148 @@ def index():
 @app.route('/managerindex')
 @login_required
 def managerindex():
-    home={'description':'Welcome.'}
-    return render_template('managerHome.html', title='Home', home=home, account=current_user)
+    if current_user.role == 2:
+        home={'description':'Welcome.'}
+        return render_template('managerHome.html', title='Home', home=home, account=current_user)
+    else:
+        flash('You are not authorised to view this page', category='error')
+        return redirect(url_for('index'))
+
+
+
+
 
 @app.route('/addscooters', methods=['GET', 'POST'])
 def addscooters():
-    form = ScooterForm()
-    if form.validate_on_submit():
-        nlocation=request.form.get("location")
-        new_scooter=Scooter(location = nlocation, availability = True)
-        db.session.add(new_scooter)
-        db.session.commit()
-        flash("Scooter added")
-        return redirect(url_for("addscooters"))
+    if current_user.role == 2:
+        form = ScooterForm()
+        if form.validate_on_submit():
+            nlocation=request.form.get("location")
+            new_scooter=Scooter(location = nlocation, availability = True)
+            db.session.add(new_scooter)
+            db.session.commit()
+            flash("Scooter added")
+            return redirect(url_for("addscooters"))
+        return render_template('addscooters.html', title='Scooters', form=form)
+    else:
+        flash('You are not authorised to view this page', category='error')
+        return redirect(url_for('index'))
 
-    return render_template('addscooters.html', title='Scooters', form=form)
 
 @app.route('/addoptions', methods=['GET', 'POST'])
 def addoptions():
-    form = OptionsForm()
-    if form.validate_on_submit():
-        nhours=request.form.get("hours")
-        nprice=request.form.get("price")
-        new_option=Options(hours = nhours, price = nprice)
-        db.session.add(new_option)
-        db.session.commit()
-        flash("Option added")
-        return redirect(url_for("addoptions"))
-
-    return render_template('addoptions.html', title='Options', form=form)
+    if current_user.role == 2:
+        form = OptionsForm()
+        if form.validate_on_submit():
+            nhours = request.form.get("hours")
+            nprice = request.form.get("price")
+            new_option = Options(hours=nhours, price=nprice)
+            db.session.add(new_option)
+            db.session.commit()
+            flash("Option added")
+            return redirect(url_for("addoptions"))
+        return render_template('addoptions.html', title='Options', form=form)
+    else:
+        flash('You are not authorised to view this page', category='error')
+        return redirect(url_for('index'))
 
 @app.route('/viewscooters', methods=['GET', 'POST'])
 def viewscooters():
     scooters = Scooter.query.all()
     return render_template('viewscooters.html', title='Scooters', scooters=scooters)
 
+
+
 @app.route('/book_scooter/<int:scooter_id>', methods=['GET', 'POST'])
 def mark_task(scooter_id):
-    options = Options.query.all()
-    return render_template('options.html', title='Options', options=options, id = scooter_id)
+    if current_user.role == 1:
+        options = Options.query.all()
+        return render_template('options.html', title='Options', options=options, id=scooter_id)
+    else:
+        flash('Our services are available to our customers only', category='error')
+        return redirect(url_for('managerindex'))
 
 @app.route('/book_scooter/<int:scooter_id>/<int:option_id>', methods=['GET', 'POST'])
 def book_scooter(scooter_id, option_id):
-    #get current date
-    cDate = dt.date.today()
-    strDate = cDate.strftime("%D")#convert into string
+    if current_user.role == 1:
+        #get current date
+        cDate = dt.date.today()
+        strDate = cDate.strftime("%D")#convert into string
 
-    scooter = Scooter.query.get(scooter_id)
-    scooter.availability = False
-    bookingOption = Options.query.get(option_id)
-    userId = current_user.get_id()
-    newBooking = Booking(customerId = userId, scooterId = scooter.id, price = bookingOption.price, hours = bookingOption.hours, date=strDate)
-    db.session.add(newBooking)
-    db.session.commit()
-    flash("Scooter booked")
-    return redirect(url_for("payment"))
+        scooter = Scooter.query.get(scooter_id)
+        scooter.availability = False
+        bookingOption = Options.query.get(option_id)
+        userId = current_user.get_id()
+        newBooking = Booking(customerId = userId, scooterId = scooter.id, price = bookingOption.price, hours = bookingOption.hours, date=strDate)
+        db.session.add(newBooking)
+        db.session.commit()
+        flash("Scooter booked")
+        return redirect(url_for("payment"))
+    else:
+        flash('Our services are available to our customers only', category='error')
+        return redirect(url_for('managerindex'))
 
 @app.route('/book_scooter/confirmation_page', methods=['GET', 'POST'])
 def confirmation_page():
-    customerId = current_user.get_id()
-    booking = Booking.query.filter_by(customerId=customerId).order_by(Booking.bookingId.desc()).first()
-    nemail = Customer.query.get(customerId)
-    msg = Message('Booking Confirmation', sender =   'raja@mailtrap.io', recipients = [nemail.email])
-    msg.body = 'Hello ' + str(nemail.email) + '\nBooking ID: '  + str(booking.bookingId) + '\nCustomer ID: ' + str(booking.customerId) + '\nScooter ID: ' + str(booking.scooterId) + '\nPrice: ' + str(booking.price) + '\nHourse: ' + str(booking.hours)
-    mail.send(msg)
-    return render_template('bookingConfirmation.html', title='Confirmation', booking = booking)
+    if current_user.role == 1:
+        customerId = current_user.get_id()
+        booking = Booking.query.filter_by(customerId=customerId).order_by(Booking.bookingId.desc()).first()
+        nemail = Account.query.get(customerId)
+        msg = Message('Booking Confirmation', sender =   'raja@mailtrap.io', recipients = [nemail.email])
+        msg.body = 'Hello ' + str(nemail.email) + '\nBooking ID: '  + str(booking.bookingId) + '\nCustomer ID: ' + str(booking.customerId) + '\nScooter ID: ' + str(booking.scooterId) + '\nPrice: ' + str(booking.price) + '\nHourse: ' + str(booking.hours)
+        mail.send(msg)
+        return render_template('bookingConfirmation.html', title='Confirmation', booking = booking)
+    else:
+        flash('Our services are available to our customers only', category='error')
+        return redirect(url_for('managerindex'))
 
 @app.route('/payment', methods=['GET', 'POST'])
 def payment():
-    form = PaymentForm()
-    if form.validate_on_submit():
-        flash("Payment Succesful")
-        return redirect(url_for("confirmation_page"))
-    return render_template('payment.html', title='Payment', form=form)
+    if current_user.role == 1:
+        form = PaymentForm()
+        if form.validate_on_submit():
+            flash("Payment Succesful")
+            return redirect(url_for("confirmation_page"))
+        return render_template('payment.html', title='Payment', form=form)
+    else:
+        flash('Our services are available to our customers only', category='error')
+        return redirect(url_for('managerindex'))
 
 @app.route('/add_payment_details', methods=['GET','POST'])
 def add_payment_details():
-    form = AddPaymentMethodForm()
-    if form.validate_on_submit():
-        ncardNumber=request.form.get("cardNum")
-        nexpiryDate=request.form.get("expiryDate")
-        ncardName=request.form.get("cardName")
-        new_card=PaymentCard(digit16 = ncardNumber, ExpiryDate = nexpiryDate, CardName = ncardName, CustomerId = current_user.get_id())
-        db.session.add(new_card)
-        db.session.commit()
-        flash("Payment Method Added")
-        return redirect(url_for("index"))
-    return render_template('addPayment.html', title = 'Add Payment Detail', form=form)
+    if current_user.role == 1:
+        form = AddPaymentMethodForm()
+        if form.validate_on_submit():
+            ncardNumber = request.form.get("cardNum")
+            nexpiryDate = request.form.get("expiryDate")
+            ncardName = request.form.get("cardName")
+            new_card = PaymentCard(digit16=ncardNumber, ExpiryDate=nexpiryDate, CardName=ncardName,
+                                   CustomerId=current_user.get_id())
+            db.session.add(new_card)
+            db.session.commit()
+            flash("Payment Method Added")
+            return redirect(url_for("index"))
+        return render_template('addPayment.html', title='Add Payment Detail', form=form)
+    else:
+        flash('Our services are available to our customers only', category='error')
+        return redirect(url_for('managerindex'))
 
 @app.route('/send_feedback_form', methods=['GET','POST'])
 def feedback_form():
-    form=FeedbackForm()
-    if form.validate_on_submit():
-        nScooterId=request.form.get("scooterId")
-        nfeedback = request.form.get("feedback")
-        new_feedback_form=FeedbackCard(scooterId=nScooterId, feedback=nfeedback)
-        db.session.add(new_feedback_form)
-        db.session.commit()
-        flash("Feedback Form Sent")
-        return redirect(url_for("index"))
-    return render_template('FeedbackForm.html', title = 'Send Scooter Feedback', form=form)
+    if current_user.role == 1:
+        form = FeedbackForm()
+        if form.validate_on_submit():
+            nScooterId = request.form.get("scooterId")
+            nfeedback = request.form.get("feedback")
+            new_feedback_form = FeedbackCard(scooterId=nScooterId, feedback=nfeedback)
+            db.session.add(new_feedback_form)
+            db.session.commit()
+            flash("Feedback Form Sent")
+            return redirect(url_for("index"))
+        return render_template('FeedbackForm.html', title='Send Scooter Feedback', form=form)
+    else:
+        flash('Our services are available to our customers only', category='error')
+        return redirect(url_for('managerindex'))
 
 @app.route('/revenue', methods=['GET', 'POST'])
 def revenue_page():
